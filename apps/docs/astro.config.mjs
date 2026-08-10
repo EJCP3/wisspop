a18665979837883e2d1922e6d7b2d077c92ccd1b
EJCP3 <euddycabrera03@gmail.com>
2026-08-10 16:34:02 -0400

    fix: actualizar componentes astro y dependencias de wisspop v0.1.1

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -4,5 +4,10 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wisspop.vercel.app',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    ssr: {
+      noExternal: ['wisspop']
+    }
+  }
 });
